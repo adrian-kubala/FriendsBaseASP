f270cfa558f94b772c0a2ae28ef064cf8a8ec662
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-19 19:33:41 +0200

    Create model called Friend

---
+++ FriendsBaseASP/Models/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FriendsBaseASP.Models
+{
+    public class Friend
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public List<Friend> AllFriends { get; set; }
+    }
+}

3e0bd9f953700e1f5a8ab677173461f2fbdf246c
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-20 00:01:30 +0200

    Implement Insert method

---
+++ FriendsBaseASP/DataAccess/DataAccessLayer.cs
@@ -0,0 +1,45 @@
+﻿using FriendsBaseASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace FriendsBaseASP.DataAccess
+{
+    public class DataAccessLayer
+    {
+        public string InsertData(Friend friend)
+        {
+
+            SqlConnection con = null;
+            string result = "";
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete_Customer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id_znajomego", 0);
+                cmd.Parameters.AddWithValue("@Imie_znajomego", friend.name);
+                cmd.Parameters.AddWithValue("@Nazwisko_znajomego", friend.lastName);
+                cmd.Parameters.AddWithValue("@Query", 1);
+
+                con.Open();
+                result = cmd.ExecuteScalar().ToString();
+                return result;
+            }
+            catch
+            {
+                return result = "";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+    }
+}

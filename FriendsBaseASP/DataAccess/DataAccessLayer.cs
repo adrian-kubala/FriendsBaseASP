c16ce517021a7517dc545612ceff41f727654567
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-20 00:12:48 +0200

    Implement SelectDataById method

--- FriendsBaseASP/DataAccess/DataAccessLayer.cs
+++ FriendsBaseASP/DataAccess/DataAccessLayer.cs
@@ -121,6 +121,7 @@ namespace FriendsBaseASP.DataAccess
                 da.SelectCommand = cmd;
                 ds = new DataSet();
                 da.Fill(ds);
+
                 friends = new List<Friend>();
                 for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
                 {
@@ -142,6 +143,47 @@ namespace FriendsBaseASP.DataAccess
                 con.Close();
             }
         }
+
+        public Friend SelectDataById(string friendId)
+        {
+            SqlConnection con = null;
+            DataSet ds = null;
+            Friend friend = null;
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete_Customer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id_znajomego", friendId);
+                cmd.Parameters.AddWithValue("@Imie_znajomego", null);
+                cmd.Parameters.AddWithValue("@Nazwisko_znajomego", null);
+                cmd.Parameters.AddWithValue("@Query", 5);
+
+                SqlDataAdapter da = new SqlDataAdapter();
+                da.SelectCommand = cmd;
+                ds = new DataSet();
+                da.Fill(ds);
+
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    friend = new Friend();
+                    friend.id = Convert.ToInt32(ds.Tables[0].Rows[i]["Id_znajomego"].ToString());
+                    friend.name = ds.Tables[0].Rows[i]["Imie_znajomego"].ToString();
+                    friend.lastName = ds.Tables[0].Rows[i]["Nazwisko_znajomego"].ToString();
+                }
+                return friend;
+            }
+            catch
+            {
+                return friend;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
 
     }
 }

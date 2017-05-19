9af615fc0506ef3c4cf39aec5a95dfad14002930
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-19 19:37:56 +0200

    Set model annotations

--- FriendsBaseASP/Models/Friend.cs
+++ FriendsBaseASP/Models/Friend.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,15 @@ namespace FriendsBaseASP.Models
 {
     public class Friend
     {
+        [Key]
         public int id { get; set; }
+
+        [Required(ErrorMessage ="Wprowadź imię")]
         public string name { get; set; }
+
+        [Required(ErrorMessage = "Wprowadź nazwisko")]
         public string lastName { get; set; }
+
         public List<Friend> AllFriends { get; set; }
     }
 }

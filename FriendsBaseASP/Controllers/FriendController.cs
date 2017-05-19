2fd4b389fdf73111da1abf4fdd76ddb1d5bc293c
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-20 00:18:53 +0200

    Implement InsertFriend in the Controller

--- FriendsBaseASP/Controllers/FriendController.cs
+++ FriendsBaseASP/Controllers/FriendController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FriendsBaseASP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,7 +10,14 @@ namespace FriendsBaseASP.Controllers
     public class FriendController : Controller
     {
         // GET: Friend
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertFriend(Friend friend)
         {
             return View();
         }

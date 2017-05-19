a783f7f2d018e95dd9d81bd75021d19ea747c8f7
Adrian Kubala <adrian.kubala@gmail.com>
2017-05-20 00:06:13 +0200

    Implement Delete method

--- FriendsBaseASP/DataAccess/DataAccessLayer.cs
+++ FriendsBaseASP/DataAccess/DataAccessLayer.cs
@@ -70,5 +70,34 @@ namespace FriendsBaseASP.DataAccess
             }
         }
 
+        public string DeleteData(Friend friend)
+        {
+            SqlConnection con = null;
+            string result = "";
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["mycon"].ToString());
+                SqlCommand cmd = new SqlCommand("Usp_InsertUpdateDelete_Customer", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@Id_znajomego", friend.id);
+                cmd.Parameters.AddWithValue("@Imie_znajomego", null);
+                cmd.Parameters.AddWithValue("@Nazwisko_znajomego", null);
+                cmd.Parameters.AddWithValue("@Query", 3);
+
+                con.Open();
+                result = cmd.ExecuteScalar().ToString();
+                return result;
+            }
+            catch
+            {
+                return result = "";
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
     }
 }
